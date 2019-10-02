533d79d8138848fc72f173072e7b7c36329e4b9d
Camilo Cortes <ccortes2@hawk.iit.edu>
2019-10-02 12:50:57 -0500

    added glow effect to plants when mouse hovers over button
    added glow id to plant image
    added javascript functions for when mouse enters and exits submit button area
    created javascript functions for these additions

---
+++ js/site.js
@@ -0,0 +1,7 @@
+function makeGlow() {
+  document.getElementById("glow").style.boxShadow = "10px 20px 30px gold";
+}
+
+function stopGlow() {
+	document.getElementById("glow").style.boxShadow = "0px 0px 0px"
+}
